71088f152328c67bd106eeae1dd93f6013c2e2cc
Jim Zhang <jim.zoumo@gmail.com>
2017-01-18 10:49:46 +0800

    refactor: move isDigit to json.go

--- json.go
+++ json.go
@@ -942,3 +942,11 @@ func methodName() string {
 	}
 	return f.Name()
 }
+
+func isDigit(c byte) bool {
+	switch c {
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return true
+	}
+	return false
+}
